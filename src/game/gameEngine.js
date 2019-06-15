ba01490b39e37f0a6a44f7c1387bde166ee043e5
Julien Lengrand-Lambert <julien@lengrand.fr>
2019-06-15 16:17:27 +0200

    chore: add dev tools support

--- src/game/gameEngine.js
+++ src/game/gameEngine.js
@@ -11,7 +11,11 @@ const app = combineReducers({
   gamesReducers,
 });
 
-const store = createStore(app, initialState);
+const store = createStore(
+  app,
+  initialState,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(), // dev tools
+);
 
 console.log(store.getState());
 const unsubscribe = store.subscribe(() => console.log(store.getState()));

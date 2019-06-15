3766b48ffd2ee3efcb1a1a6e17526bfed7644bc7
Julien Lengrand-Lambert <julien@lengrand.fr>
2019-06-15 15:22:11 +0200

    feat: set redux up

--- src/game/gameEngine.js
+++ src/game/gameEngine.js
@@ -1,5 +1,6 @@
-import { configureStore, createSlice } from 'redux-starter-kit';
-import { isMainThread } from 'worker_threads';
+import { combineReducers, createStore } from 'redux';
+
+const STOP = 'STOP';
 
 const ONGOING = 'ONGOING';
 const STOPPED = 'STOPPED';
@@ -10,28 +11,29 @@ const initialState = {
   },
 };
 
-const gamesSlice = createSlice({
-  initialState: {
-    status: ONGOING,
-  },
-  reducers: {
-    stop: (state, action) => {
-      state.status = STOPPED;
-    },
-  },
+function stop() {
+  return { type: STOP };
+}
+
+function gamesReducer(state = {}, action) {
+  switch (action.type) {
+    case STOP:
+      return Object.assign({}, state, {
+        status: STOPPED,
+      });
+    default:
+      return state;
+  }
+}
+
+const app = combineReducers({
+  gamesReducer,
 });
 
-const { gamesActions, gamesReducer } = gamesSlice;
+const store = createStore(app, initialState);
 
-// Setting up the store (just like the model in Elm)
-const store = configureStore({
-  // all possible actions on the state
-  reducer: {
-    game: gamesReducer,
-  },
-  preloadedState: initialState, // default initial state
-});
+console.log(store.getState());
+const unsubscribe = store.subscribe(() => console.log(store.getState()));
+store.dispatch(stop());
 
-console.log(store);
-store.dispatch(gamesActions.stop());
-console.log(store);
+unsubscribe();

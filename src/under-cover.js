f97444f541b877041ae1fa35ffabfac91cb62861
Julien Lengrand-Lambert <julien@lengrand.fr>
2019-06-24 22:56:34 +0200

    feat: can add players to a list

--- src/under-cover.js
+++ src/under-cover.js
@@ -2,15 +2,19 @@ import { LitElement, html } from 'lit-element';
 import { connect } from 'pwa-helpers';
 import { store } from './game/gameStore';
 import { createGame } from './game/gameActions';
+import '@vaadin/vaadin-button';
+import '@vaadin/vaadin-text-field';
 
 class UnderCover extends connect(store)(LitElement) {
   static get properties() {
-    return {};
+    return {
+      players: { type: Array },
+    };
   }
 
   constructor() {
     super();
-    this.players = ['Bob', 'Gerard'];
+    this.players = ['Bob'];
     this.game = undefined;
   }
 
@@ -22,10 +26,39 @@ class UnderCover extends connect(store)(LitElement) {
     store.dispatch(createGame(this.players));
   }
 
+  addFriend() {
+    const newFriend = this.shadowRoot.querySelector('#friend-text-field').value;
+    this.players = [...this.players, newFriend];
+    this.shadowRoot.querySelector('#friend-text-field').value = '';
+  }
+
   render() {
     return html`
-      <h1>Title</h1>
-      <button @click=${this.startGame}>Create Game!</button>
+      <div class="newGame">
+        <div class="intro">
+          <h1>UnderCover</h1>
+          <p>Get a challenge, kill your friends, win the game . . . during dinner!</p>
+        </div>
+        <div class="addFriends">
+          <vaadin-text-field
+            id="friend-text-field"
+            aria-placeholder="player name"
+            aria-label="Player Name"
+          ></vaadin-text-field>
+          <vaadin-button @click=${this.addFriend}>+</vaadin-button>
+        </div>
+        <div class="friendsList">
+          <ul>
+            ${this.players.map(
+              player =>
+                html`
+                  <li>${player}</li>
+                `,
+            )}
+          </ul>
+          <vaadin-button @click=${this.startGame}>Create Game!</vaadin-button>
+        </div>
+      </div>
     `;
   }
 }
